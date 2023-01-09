480c423d03e0bf92370911252a58fe68f517f44c
Simon Banyard <simon@banyard.org>
2023-01-09 11:55:37 +0000

    Removed config.json. Added config.psd1. Renamed release.ps1

--- Release-Messages.ps1
+++ Release-Messages.ps1
@@ -1,19 +1,21 @@
 # Load config file
-$config = Get-Content -Path config.json | ConvertFrom-Json
+$config = Import-PowerShellDataFile -Path .\config.psd1
 
 # Setup required variables
-$baseUrl = "https://" + $config.region + "-api.mimecast.com"
+$baseUrl = $config.baseURL
 $uri = "/api/gateway/get-hold-message-list"
 $url = $baseUrl + $uri
-$accessKey = $config.access_key
-$secretKey = $config.secret_key
-$appId = $config.app_id
-$appKey = $config.app_key
+$accessKey = $config.accessKey
+$secretKey = $config.secretKey
+$appId = $config.appId
+$appKey = $config.appKey
 
 
 # Generate request header values
 $hdrDate = (Get-Date).ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss UTC")
 $requestId = [guid]::NewGuid().guid
+$endTime = (Get-Date).ToUniversalTime().ToString("yyyy-mm-ddThh:mm:ss+0000")
+$startTime = (Get-Date).AddHours(-1).ToUniversalTime().ToString("yyyy-mm-ddThh:mm:ss+0000")
 
 # Create the HMAC SHA1 of the Base64 decoded secret key for the Authorization header
 $sha = New-Object System.Security.Cryptography.HMACSHA1
@@ -23,10 +25,11 @@ $sig = [Convert]::ToBase64String($sig)
 
 # Create Headers
 $headers = @{"Authorization" = "MC " + $accessKey + ":" + $sig;
-                "x-mc-date" = $hdrDate;
-                "x-mc-app-id" = $appId;
-                "x-mc-req-id" = $requestId;
-                "Content-Type" = "application/json"}
+    "x-mc-date"              = $hdrDate;
+    "x-mc-app-id"            = $appId;
+    "x-mc-req-id"            = $requestId;
+    "Content-Type"           = "application/json"
+}
 
 # Create post body
 $postBody = "{
@@ -38,23 +41,25 @@ $postBody = "{
                 ""data"": [
                     {
                         ""admin"": true,
-                        ""start"": ""2022-12-05T14:49:18+0000"",
-                        ""end"": ""2022-12-10T14:49:18+0000""
+                        ""start"": ""$startTime"",
+                        ""end"": ""$endTime""
                     }
                 ]
             }"
+
+Write-Output $postBody
 
 # Send Request
 $response = Invoke-RestMethod -Method Post -Headers $headers -Body $postBody -Uri $url
 
 # Initialise array to hold message id
-$message_to_release = @()
+$messageToRelease = @()
 
 # Loop over response to get message ids to release
 foreach ($item in $response.data) {
     # Replace $item.reasonCode with the reason that needs to be bulk released
     if ($item.reasonCode -contains "default_inbound_attachment_protect_definition") {
-        $message_to_release += $item.id
+        $messageToRelease += $item.id
     }
 }
 
@@ -63,7 +68,7 @@ $uri = "/api/gateway/hold-release"
 $url = $baseUrl + $uri
 
 # Loop over messages_to_release and release each message
-foreach ($message in $message_to_release) {
+foreach ($message in $messageToRelease) {
     # Create the HMAC SHA1 of the Base64 decoded secret key for the Authorization header
     $sha = New-Object System.Security.Cryptography.HMACSHA1
     $sha.key = [Convert]::FromBase64String($secretKey)
@@ -72,10 +77,11 @@ foreach ($message in $message_to_release) {
 
     # Create Headers
     $headers = @{"Authorization" = "MC " + $accessKey + ":" + $sig;
-    "x-mc-date" = $hdrDate;
-    "x-mc-app-id" = $appId;
-    "x-mc-req-id" = $requestId;
-    "Content-Type" = "application/json"}
+        "x-mc-date"              = $hdrDate;
+        "x-mc-app-id"            = $appId;
+        "x-mc-req-id"            = $requestId;
+        "Content-Type"           = "application/json"
+    }
 
     #Create post body
     $postBody = "{
---
+++ config.psd1
@@ -0,0 +1,12 @@
+# Keys are obtained from your Mimecast account, see
+# https://community.mimecast.com/s/article/Managing-API-Applications-505230018
+# See https://integrations.mimecast.com/documentation/api-overview/global-base-urls/ for a list of Base URLs
+# Use the Base URL that corresponds to the location of your account
+
+@{
+    accessKey = "test"
+    appId     = "123456"
+    appKey    = "7890"
+    baseURL   = "https://example.com"
+    secretKey = "tested"
+}
